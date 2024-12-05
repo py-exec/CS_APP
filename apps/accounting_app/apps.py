f246c075e77c39ec16f59b82cac87420c3b90ff9
py <pedram.yavari72@gmail.com>
2024-12-05 17:51:21 +0330

    3

--- apps/accounting_app/apps.py
+++ apps/accounting_app/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AccountingAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'accounting_app'
+    name = 'apps.accounting_app'
