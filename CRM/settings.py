c7408f635ea43cb961ec4eb224fa07e4c7513d06
py <pedram.yavari72@gmail.com>
2024-12-06 15:44:10 +0330

    6

--- CRM/settings.py
+++ CRM/settings.py
@@ -89,11 +89,9 @@ CACHES = {
         'BACKEND': 'django.core.cache.backends.redis.RedisCache',
         'LOCATION': config('REDIS_URL', default='redis://127.0.0.1:6379/1'),
         'TIMEOUT': 300,
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-        },
     }
 }
+
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
 SESSION_CACHE_ALIAS = 'default'
